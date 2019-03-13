5f7bf05204c09f44e615666764a066e6307db3a6
zgy <gy.zhang@haitongwangxiao.com>
2019-03-13 18:44:22 +0800

    first commit

---
+++ src/components/banner/articleBanner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bannerBox" v-if="adPass ? adPass.length > 0 : adPass">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(item, key) in adPass" :key="key">
+        <a :href="item.url"><img style="width: 3.45rem; height: 2.14rem;" :src="item.photo" alt=""></a>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script type='text/ecmascript-6'>
+export default {
+
+}
+</script>
+
+<style scoped lang='less'>
+  .bannerBox {
+    height: 2.14rem;
+    width: 7.1rem;
+    margin: 0 auto;
+    padding-top: .3rem;
+    position: relative;
+    overflow: hidden;
+    img {
+      height: 100%;
+      width: 100%;
+    }
+  }
+</style>
---
+++ src/components/banner/banner.vue
@@ -0,0 +1,34 @@
+<template>
+    <mt-swipe :auto="time">
+      <mt-swipe-item v-for = "(item, key) in banner" v-show = "item['position'] *1 == 10" :key="key">
+        <a :href="item.url ? item.url : 'javascript:;'">
+          <img :src="item.photo" :alt="item.title">
+        </a>
+      </mt-swipe-item>
+    </mt-swipe>
+</template>
+
+<script>
+export default {
+  props: {
+    time: {
+      default: 3000,
+      type: Number
+    },
+    banner: {
+      defautl: () => new Array()
+    }
+  }
+}
+</script>
+
+<style>
+  .mint-swipe .mint-swipe-indicators .mint-swipe-indicator {
+    bottom: .23rem;
+    opacity: 1;
+    background: #fff;
+  }
+  .mint-swipe .mint-swipe-indicators .is-active  {
+    background: #e10030;
+  }
+</style>
